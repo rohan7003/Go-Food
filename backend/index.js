1dca4711fcb489af0b03d729578fcc8524448102
Rohan Patil <rohanrpatil7003@gmail.com>
2026-04-27 18:00:05 +0530

    Fixed CORS

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 const mongoDB = require("./db");
 const cors = require('cors');
-app.use(cors());
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 // Connect to MongoDB and fetch data
 mongoDB();
